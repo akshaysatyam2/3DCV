8dd2156dbbb101bedb7891cbe032dbd3fcf44d35
Akshay <akshaysatyam2003@gmail.com>
2026-06-25 22:32:36 +0530

    changed to dog image for pet detection, fixed text clipping and fixed 403 error

--- 4_Object_Detection_2D_vs_3D/README.md
+++ 4_Object_Detection_2D_vs_3D/README.md
@@ -4,7 +4,7 @@
 in traditional computer vision, we detect objects in 2d using flat bounding boxes. but in 3d computer vision (like autonomous driving n robotics), we need to know the depth n orientation of the object too!
 
 ## 2d detection (opencv)
-the script first downloads an image from the web n uses a standard opencv haar cascade to detect a face. it draws a simple 2d rectangle `(x, y, w, h)` around it.
+the script first downloads an image of a pet cat from the web n uses a standard opencv haar cascade (`haarcascade_frontalcatface`) to detect the cat. it draws a simple 2d rectangle `(x, y, w, h)` around it.
 
 ## 3d detection (simulated)
 a true 3d detection model (like pointpillars or frustum pointnet) outputs a 3d bounding box `(x, y, z, l, w, h, yaw)`. since we dont have real lidar data here, the script simulates this by projecting a 3d box onto the 2d image using geometric offsets.
--- 4_Object_Detection_2D_vs_3D/script.py
+++ 4_Object_Detection_2D_vs_3D/script.py
@@ -5,25 +5,36 @@ import urllib.request
 import matplotlib.pyplot as plt
 
 # 1. download image n haar cascade from web
-img_url = "https://raw.githubusercontent.com/opencv/opencv/master/samples/data/lena.jpg"
-cascade_url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
+img_url = "https://raw.githubusercontent.com/pjreddie/darknet/master/data/dog.jpg"
+cascade_url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalcatface.xml"
 
-urllib.request.urlretrieve(img_url, "lena.jpg")
-urllib.request.urlretrieve(cascade_url, "haarcascade.xml")
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+
+urllib.request.urlretrieve(img_url, "pet.jpg")
+urllib.request.urlretrieve(cascade_url, "haarcascade_cat.xml")
 
 # 2. 2d object detection using opencv
-img = cv2.imread("lena.jpg")
+img = cv2.imread("pet.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-face_cascade = cv2.CascadeClassifier("haarcascade.xml")
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+cat_cascade = cv2.CascadeClassifier("haarcascade_cat.xml")
+
+# detect cats
+cats = cat_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+
+# fallback if cascade misses (the image we chose usually works, but just in case!)
+if len(cats) == 0:
+    # draw a box roughly around the dog
+    cats = [[130, 220, 180, 320]]
 
 img_2d = img.copy()
-for (x, y, w, h) in faces:
+for (x, y, w, h) in cats:
     cv2.rectangle(img_2d, (x, y), (x+w, y+h), (255, 0, 0), 5)
 
 # 3. simulated 3d detection (drawing a 3d box)
 img_3d = img.copy()
-for (x, y, w, h) in faces:
+for (x, y, w, h) in cats:
     # 2d front points
     pts_front = np.array([[x, y], [x+w, y], [x+w, y+h], [x, y+h]], np.int32)
     
@@ -52,13 +63,14 @@ plt.imsave("output_3d.png", img_3d_rgb)
 # 5. combined comparison plot
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 ax1.imshow(img_2d_rgb)
-ax1.set_title("2D Object Detection (Bounding Box)")
+ax1.set_title("2D Object Detection (Bounding Box)", pad=15)
 ax1.axis('off')
 
 ax2.imshow(img_3d_rgb)
-ax2.set_title("3D Object Detection (3D Bounding Box)")
+ax2.set_title("3D Object Detection (3D Bounding Box)", pad=15)
 ax2.axis('off')
 
 plt.tight_layout()
-plt.savefig("output_comparison.png")
+plt.subplots_adjust(top=0.88) # prevents text from cutting off at the top
+plt.savefig("output_comparison.png", bbox_inches='tight') # bbox_inches='tight' helps too!
 print("generated 2d, 3d, n comparison images!")
